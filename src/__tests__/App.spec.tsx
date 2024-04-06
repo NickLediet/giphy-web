bd7462c1a2224f98441ee9d162314b7b2872aa44
Nicholas Lediet <nicklediet@gmail.com>
2024-04-06 15:07:29 -0400

    Add initial jsdom based test with test utils

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,7 +1,9 @@
 import { describe, expect, it } from 'vitest';
-
+import { render, screen } from '@testing-library/react';
+import App from '../App';
 describe('App', () => {
-  it('should render', () => {
-    expect(true).toBe(true);
+  it('should render the correct text', () => {
+    render(<App />);
+    expect(screen.getByText('count is 0')).not.toBeNull();
   });
 });
